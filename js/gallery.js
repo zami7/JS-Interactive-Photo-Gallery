e28b4c29ff142209f0d9f1d16f0186e464efcfeb
Zamil Rahman <102779273+zami7@users.noreply.github.com>
2025-02-21 14:53:32 +0530

    project upload

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,13 @@
-
+function upDate(previewPic){
+    let imageDiv = document.getElementById("image");
+    imageDiv.style.backgroundImage = `url(${previewPic.src})`;
+    imageDiv.textContent = previewPic.alt;
+      {console.log("Mouse is over the image");}    
+    }
+    
+function unDo() {   
+    let imageDiv = document.getElementById("image");
+    imageDiv.style.backgroundImage = "url('images/bg.jpg')"; 
+    imageDiv.textContent = "Hover over an image below to display here.";
+      {console.log("Mouse is out of the image");}
+    }	
